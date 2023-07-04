e8155cfb16521c34d1676e87b0bd70e0b6cfadab
holicmiku <holicmiku.15@gmail.com>
2023-07-04 21:15:00 +0900

    Permutation / Combination / SubSet Practice

---
+++ 20230704.cpp
@@ -0,0 +1,93 @@
+﻿#include<iostream>
+
+#define N 3
+#define R 2
+
+using namespace std;
+
+bool isSelected[N] = { false, };
+int answer[R];
+int number[N];
+
+void PrintArray() {
+	for (int i = 0; i < R; i++) {
+		cout << answer[i] << " ";
+	}
+	cout << "\n";
+}
+
+void Permutation(int depth) {
+	if (depth == R) {
+		PrintArray();
+		return;
+	}
+
+	for (int i = 0; i < N; i++) {
+		if (!isSelected[i]) {
+			isSelected[i] = true;
+			answer[depth] = i + 1;
+			Permutation(depth + 1);
+			isSelected[i] = false;
+		}
+	}
+}
+
+void DuplicatePermutation(int depth) {
+	if (depth == R) {
+		PrintArray();
+		return;
+	}
+
+	for (int i = 0; i < N; i++) {
+		answer[depth] = i + 1;
+		Permutation(depth + 1);
+	}
+}
+
+void Combination(int depth, int next) {
+	if (depth == R) {
+		PrintArray();
+		return;
+	}
+
+	for (int i = next; i < N; i++) {
+		answer[depth] = i+1;
+		Combination(depth + 1, i + 1);
+	}
+}
+
+void DuplicateCombination(int depth, int next) {
+	if (depth == R) {
+		PrintArray();
+		return;
+	}
+
+	for (int i = next; i < N; i++) {
+		answer[depth] = i + 1;
+		Combination(depth + 1, i);
+	}
+}
+
+void SubSet() {
+	for (int i = 0; i < (1 << N); i++) {
+		for (int j = 0; j < N; j++) {
+			if (i & (1 << j)) {
+				cout << number[j] << " ";
+			}
+		}
+		cout << "\n";
+	}
+}
+
+int main() {
+
+	for (int i = 0; i < N; i++) {
+		number[i] = i + 1;
+	}
+
+	//Permutation(0);
+	//DuplicatePermutation(0);
+	//Combination(0, 0);
+	//DuplicateCombination(0, 0);
+	SubSet();
+}
